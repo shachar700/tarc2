e5c2d4a798d3b9953e87e18a9e1e4b232a1e6526
shachar700 <shachar700@gmail.com>
2025-08-19 12:03:28 +0300

    Added mus_intro_mystery song

--- src/main_menu.c
+++ src/main_menu.c
@@ -1278,7 +1278,7 @@ static void Task_NewGameBirchSpeech_Init(u8 taskId)
     gTasks[taskId].tPlayerSpriteId = SPRITE_NONE;
     gTasks[taskId].data[3] = 0xFF;
     gTasks[taskId].tTimer = 0xD8;
-    PlayBGM(MUS_ROUTE122);
+    PlayBGM(MUS_INTRO_MYSTERY/*MUS_ROUTE122*/);
     ShowBg(0);
     ShowBg(1);
 }
